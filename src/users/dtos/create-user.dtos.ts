c1ffbc0855e69d759af5d0d37ff98a26542ea2f8
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2023-09-17 17:54:01 +0100

    CRUD

--- src/users/dtos/create-user.dtos.ts
+++ src/users/dtos/create-user.dtos.ts
@@ -1,8 +1,32 @@
-import { IsEmail, IsString } from "class-validator";
+import { IsEmail, IsString } from 'class-validator';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 export class CreateUserDtos {
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @IsEmail()
   email: string;
   @IsString()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with Id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with is', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './users.entity';
@@ -7,9 +7,39 @@ import { User } from './users.entity';
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-  create(email: string, password: string) {
+  async create(email: string, password: string) {
     const user = this.repo.create({ email, password });
 
     return this.repo.save(user);
+  }
+
+  async findUserById(id: number) {
+    return await this.repo.findOneBy({ id: id });
+  }
+
+  async findByEmail(email: string) {
+    return await this.repo.find({
+      where: {
+        email: email,
+      },
+    });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.repo.findOneBy({ id: id });
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.repo.findOneBy({ id: id });
+    if (!user) {
+      throw new Error('user not found');
+    }
+    return this.repo.remove(user);
   }
 }
